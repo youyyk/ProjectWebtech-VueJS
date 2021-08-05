4ef73c000613ec2af143eadc1d46e6ce8883c874
Kaiwa??? <sethawut.p@ku.th>
2021-08-05 17:47:41 +0700

    Change location signup file

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -54,28 +54,32 @@ export default {
         },
         async signUp(){
             // let res = await AuthService.register(this.form)
-            let res = await AuthUser.dispatch('signUp',this.form)
-            if(res.success){
-                this.$swal("sign up Success", `Welcome ${res.user.username}`, "success")
-                this.$router.push("/")
-            } 
-            else {
-                this.$swal("sign up Failed", res.message, "error")
+            if(this.form.password === this.form.confirmpassword){
+                let res = await AuthUser.dispatch('signUp',this.form)
+                if(res.success){
+                    this.$swal("sign up Success", `Welcome ${res.user.username}`, "success")
+                    this.$router.push("/")
+                } 
+                else {
+                    this.$swal("sign up Failed", res.message, "error")
+                }
             }
+            else{
+                this.$swal('Password is not match', '', "error")
+            }
+            
         }
     }
 }
 </script>
 
 <style scoped lang="scss">
-
 #FormSingUp{
     text-align: left;
     width: 35%;
     margin-left: 760px;
     // margin-top: 10px;
 }
-
 h1 {
     text-align: center;
     font-size: 40px;
@@ -84,19 +88,16 @@ h1 {
     border-bottom: 4px solid #f75726;
     margin-top: 40px;
 }
-
 label {
     color: dimgrey;
     font-size: 16px;
 }
-
 input, .signUp{
     padding: 10px;
     margin: 10px;
     margin-right: 100px;
     flex: 1;
 }
-
 button {
     background-color: #e78930; 
     border: none;
@@ -114,11 +115,9 @@ button {
     transition-duration: 0.4s;
     border-radius: 8px;
 }
-
 button:hover {
   box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
 }
-
 .signUp {
     background-image: url(	https://www.sapogo.com/Upload/Article/2021/6/bf3366dfcdca2177a5aeecde14e24350.jpg);
     position: absolute;
@@ -130,5 +129,4 @@ button:hover {
     object-fit: cover;
     display: flex;
 }
-
 </style>
